17c7a63e745d0a2f5a3abf98d8ffaa8edf4e780c
Nobodiiiii <mmu503690@gmail.com>
2026-03-22 21:43:38 +0800

    1.7.1.3 - 1.20.1 feat: 1.新增单端安装

--- Fabric/src/main/java/com/nododiiiii/ponderer/fabric/PondererFabric.java
+++ Fabric/src/main/java/com/nododiiiii/ponderer/fabric/PondererFabric.java
@@ -7,11 +7,17 @@ import com.nododiiiii.ponderer.registry.ModItems;
 import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.neoforged.fml.config.ModConfig;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
 /**
  * Fabric main entrypoint.
  */
 public class PondererFabric implements ModInitializer {
+
+    private static final ResourceLocation REQUIRED_CLIENT_CHANNEL = new ResourceLocation(Ponderer.MODID, "sync_response");
 
     @Override
     public void onInitialize() {
@@ -23,5 +29,12 @@ public class PondererFabric implements ModInitializer {
 
         // Register network
         PondererServices.NETWORK.registerPackets();
+
+        // If server has this mod, connecting clients must expose our clientbound channel.
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            if (!ServerPlayNetworking.canSend(handler.player, REQUIRED_CLIENT_CHANNEL)) {
+                handler.disconnect(Component.literal("Ponderer is required on client when installed on this server."));
+            }
+        });
     }
 }
