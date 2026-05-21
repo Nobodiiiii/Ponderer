a1743cdbc6a87453bc65bb192dbb6acea6596b47
Nobodiiiii <mmu503690@gmail.com>
2026-05-21 22:16:02 +0800

    nbt长度

--- Common/src/main/java/com/nododiiiii/ponderer/ui/catnip/PlainTextListEntry.java
+++ Common/src/main/java/com/nododiiiii/ponderer/ui/catnip/PlainTextListEntry.java
@@ -38,7 +38,7 @@ public class PlainTextListEntry extends ConfigScreenList.LabeledEntry implements
         EntryTextSupport.applyTooltip(this, labelKey, tooltipKey);
 
         this.textField = new ClippedConfigTextField(Minecraft.getInstance().font, 0, 0, 200, 20);
-        this.textField.setMaxLength(1024);
+        this.textField.setMaxLength(20000);
         EntryTextSupport.applyHint(textField, hintKey);
         this.textField.setValue(initialValue);
         this.textField.moveCursorToStart();
